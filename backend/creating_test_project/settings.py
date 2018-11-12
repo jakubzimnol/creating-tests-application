5635afc94a7e54ac43ac0151e17378d6967ffc79
jakubzimnol <jakubzimnol@gmail.com>
2018-11-12 12:15:37 +0100

    add swagger and also serializers and urls files

--- backend/creating_test_project/settings.py
+++ backend/creating_test_project/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'creating_tests_app',
+    'rest_framework_swagger',
 ]
 
 MIDDLEWARE = [
