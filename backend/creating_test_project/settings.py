7892764fdf9dc2087843661f61fef24bdbd230f3
jakubzimnol <jakubzimnol@gmail.com>
2018-11-21 12:13:38 +0100

    fix uwsgi and static files

--- backend/creating_test_project/settings.py
+++ backend/creating_test_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '^pexghaluj8*aipo)&y!t0&mbmomq_osr2q-f-t8gaf&@f^^m)'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0']
 
 
 # Application definition
@@ -78,8 +78,11 @@ WSGI_APPLICATION = 'creating_test_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('POSTGRES_DB'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT')
     }
 }
 
